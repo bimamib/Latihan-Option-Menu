eac8eb9e75b933277443af7829843ea50f000410
bimamib <bimaprast1601@gmail.com>
2023-09-09 04:25:52 +0700

    Menambahkan kode pada MenuActivity untuk menghubungkan kedua komponen

--- app/src/main/java/com/bima/myappbar/MenuActivity.kt
+++ app/src/main/java/com/bima/myappbar/MenuActivity.kt
@@ -2,10 +2,18 @@ package com.bima.myappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bima.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+        }
     }
 }
